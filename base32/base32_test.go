adbe1e6045c9f8ff0e991d0992adb8ba13bacdbd
junjuncai <15229895912@163.com>
2023-12-23 20:36:40 +0800

    add base32 codec custom encoder test.

--- base32/base32_test.go
+++ base32/base32_test.go
@@ -15,6 +15,7 @@
 package base32
 
 import (
+	"github.com/caijunjun/codec/base"
 	"reflect"
 	"testing"
 )
@@ -102,6 +103,29 @@ var (
 			name:      "base32-3",
 			plainText: []byte("这是一次 base32 编码/解码测试。"),
 			encodText: []byte("T2VPJPKOLVIBH076LIGI0OJ1EDIJ6CH0SUU9DPT0G4NUH9T3SUG83PLLHFKAV5F3G210"),
+		},
+	}
+	encoder     = "!@#$%^&*()_+:{[}]<>?/-~123456789"
+	cusCodec, _ = NewCodec(encoder, base.StdPadding)
+	testsCus    = []struct {
+		name      string
+		plainText []byte
+		encodText []byte
+	}{
+		{
+			name:      "base32-1",
+			plainText: []byte("this is encode."),
+			encodText: []byte("[</&>63!{^3>!3+[:{1~(3)["),
+		},
+		{
+			name:      "base32-2",
+			plainText: []byte("this is base32 encode."),
+			encodText: []byte("[</&>63!{^3>!2?@[{>?&:<!:-1&&55%:/1!===="),
+		},
+		{
+			name:      "base32-3",
+			plainText: []byte("这是一次 base32 编码/解码测试。"),
+			encodText: []byte("7#93?3/2-9>+<!*&->]>!2?@[{>?&:<!688){37!]%18<)7$68]($3--<}/_9^}$]#@!===="),
 		},
 	}
 )
@@ -243,6 +267,44 @@ func TestBase32RawHexCodec_Decode(t *testing.T) {
 	for _, tt := range testsRawHex {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := RawHexCodec.Decode(tt.encodText)
+			if err != nil {
+				t.Errorf("base32.Decode() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.plainText) {
+				t.Error("base32.Decode() failed!")
+			} else {
+				t.Log("base32.Decode() success!")
+			}
+			t.Logf(" got: %v", got)
+			t.Logf("want: %v", tt.plainText)
+		})
+	}
+}
+
+func TestBase32CusCodec_Encode(t *testing.T) {
+	for _, tt := range testsCus {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := cusCodec.Encode(tt.plainText)
+			if err != nil {
+				t.Errorf("base32.Encode() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.encodText) {
+				t.Error("base32.Encode() failed!")
+			} else {
+				t.Log("base32.Encode() success!")
+			}
+			t.Logf(" got: %v", got)
+			t.Logf("want: %v", tt.encodText)
+		})
+	}
+}
+
+func TestBase32CusCodec_Decode(t *testing.T) {
+	for _, tt := range testsCus {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := cusCodec.Decode(tt.encodText)
 			if err != nil {
 				t.Errorf("base32.Decode() error = %v", err)
 				return
