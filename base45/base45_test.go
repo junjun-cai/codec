74e3e9d843b49e07ac4d8482907689233cf8dba0
junjuncai <15229895912@163.com>
2024-01-02 22:33:14 +0800

    add base45 codec test.

---
+++ base45/base45_test.go
@@ -0,0 +1,82 @@
+// *********************************************************************************************************************
+// ***                                        CONFIDENTIAL --- CUSTOM STUDIOS                                        ***
+// *********************************************************************************************************************
+// * Auth: ColeCai                                                                                                     *
+// * Date: 2024/01/02 22:52:06                                                                                         *
+// * Proj: codec                                                                                                       *
+// * Pack: base45                                                                                                      *
+// * File: base45_test.go                                                                                              *
+// *-------------------------------------------------------------------------------------------------------------------*
+// * Overviews:                                                                                                        *
+// *-------------------------------------------------------------------------------------------------------------------*
+// * Functions:                                                                                                        *
+// * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
+
+package base45
+
+import (
+	"reflect"
+	"testing"
+)
+
+var (
+	testsStd = []struct {
+		name      string
+		plainText []byte
+		encodText []byte
+	}{
+		{
+			name:      "base45-1",
+			plainText: []byte("this is encode."),
+			encodText: []byte("AWE+EDH44.OE1$CUPC6VC11"),
+		},
+		{
+			name:      "base45-2",
+			plainText: []byte("this is base45 encode."),
+			encodText: []byte("AWE+EDH44.OEUJCLQE0R6D44:.DV3ERZC"),
+		},
+		{
+			name:      "base45-3",
+			plainText: []byte("这是一次 base45 编码/解码测试。"),
+			encodText: []byte("3JTNKJRDJ7-SDDG3$LA44HECXZCAW6EDTL3J4DKO26U8LVCT:IGZ.MWITV.I3BG"),
+		},
+	}
+)
+
+func TestBase45StdCodec_Encode(t *testing.T) {
+	for _, tt := range testsStd {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StdCodec.Encode(tt.plainText)
+			if err != nil {
+				t.Errorf("base45.Encode() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.encodText) {
+				t.Error("base45.Encode() failed!")
+			} else {
+				t.Log("base45.Encode() success!")
+			}
+			t.Logf(" got: %v", got)
+			t.Logf("want: %v", tt.encodText)
+		})
+	}
+}
+
+func TestBase45StdCodec_Decode(t *testing.T) {
+	for _, tt := range testsStd {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StdCodec.Decode(tt.encodText)
+			if err != nil {
+				t.Errorf("base45.Decode() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.plainText) {
+				t.Error("base45.Decode() failed!")
+			} else {
+				t.Log("base45.Decode() success!")
+			}
+			t.Logf(" got: %v", got)
+			t.Logf("want: %v", tt.plainText)
+		})
+	}
+}
