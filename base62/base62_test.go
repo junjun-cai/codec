7749997fba33309c58c0bdb0df448e2df9eb00c0
junjuncai <15229895912@163.com>
2024-01-19 22:40:52 +0800

    Update base62_test.go

--- base62/base62_test.go
+++ base62/base62_test.go
@@ -41,6 +41,29 @@ var (
 			encodText: []byte("5NZ7edvjXxa5sRWzZTZrJDqdcQo7zNxNLtzptPTTpMWAjhYgtER78b7kA"),
 		},
 	}
+	encoder     = "@#$%&*(){}ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	cusCodec, _ = NewCodec(encoder)
+	testsCus    = []struct {
+		name      string
+		plainText []byte
+		encodText []byte
+	}{
+		{
+			name:      "base62-1",
+			plainText: []byte("this is encode."),
+			encodText: []byte("rCJBhfvjUf#$Ocy$NNtO"),
+		},
+		{
+			name:      "base62-2",
+			plainText: []byte("this is base62 encode."),
+			encodText: []byte("&ZAp*fLvpSOmQjtik*&pZatNTlTiik"),
+		},
+		{
+			name:      "base62-3",
+			plainText: []byte("这是一次 base62 编码/解码测试。"),
+			encodText: []byte("*NZ)edvjXxa*sRWzZTZrJDqdcQo)zNxNLtzptPTTpMWAjhYgtER){b)kA"),
+		},
+	}
 )
 
 func TestBase62StdCodec_Encode(t *testing.T) {
@@ -66,6 +89,44 @@ func TestBase62StdCodec_Decode(t *testing.T) {
 	for _, tt := range testsStd {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := StdCodec.Decode(tt.encodText)
+			if err != nil {
+				t.Errorf("base62.Decode() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.plainText) {
+				t.Error("base62.Decode() failed!")
+			} else {
+				t.Log("base62.Decode() success!")
+			}
+			t.Logf(" got: %v", got)
+			t.Logf("want: %v", tt.plainText)
+		})
+	}
+}
+
+func TestBase62CusCodec_Encode(t *testing.T) {
+	for _, tt := range testsCus {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := cusCodec.Encode(tt.plainText)
+			if err != nil {
+				t.Errorf("base62.Encode() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.encodText) {
+				t.Error("base62.Encode() failed!")
+			} else {
+				t.Log("base62.Encode() success!")
+			}
+			t.Logf(" got: %v", got)
+			t.Logf("want: %v", tt.encodText)
+		})
+	}
+}
+
+func TestBase62CusCodec_Decode(t *testing.T) {
+	for _, tt := range testsCus {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := cusCodec.Decode(tt.encodText)
 			if err != nil {
 				t.Errorf("base62.Decode() error = %v", err)
 				return
